436491b65081ec60d07e6dd1b52f019daebe15a2
Karen Tapia <75274720+Kar3n1na@users.noreply.github.com>
2021-10-14 17:10:28 -0500

    react router installed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Provider } from 'react-redux';
 import configStore from './configStore'
 import Authentication from './components/Authentication'
+import DashBoard from './components/DashBoard';
 
 const store = configStore();
 
