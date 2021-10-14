931707f85d71d2162020b1beb5a48401deeed88d
Karen Tapia <75274720+Kar3n1na@users.noreply.github.com>
2021-10-14 16:17:19 -0500

    Authentication.js component Authentication with styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Provider } from 'react-redux';
+import configStore from './configStore'
 import Authentication from './components/Authentication'
+
+const store = configStore();
 
 const App = () => {
   return (
+    <Provider store= {store}>
     <div>
       <Authentication/>
     </div>
+    </Provider>
   )
 }
 
--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 import {
-    Button,
-    Card,
-    CardActions,
+  Button,
+  Card,
+  CardActions,
   CardContent,
   CardHeader,
   Container,
@@ -15,33 +15,67 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-
+import { Visibility } from "@mui/icons-material";
 
 const Authentication = () => {
   return (
-    <Container>
-      <Card>
-        <CardHeader></CardHeader>
+    <Container
+      sx={{
+        backgroundColor: "#0061f2",
+        minHeight: "100vh",
+        minWidth: "100%",
+        display: "flex",
+      }}
+    >
+      <Card
+        variant="outlined"
+        sx={{ width: "70%", margin: "auto", borderRadius: "8px" }}
+      >
+        <CardHeader
+          sx={{
+            textAlign: "center",
+            backgroundColor: "#F8F9F9",
+            borderBottom: "1px solid #DDDEDF",
+          }}
+          title="Login"
+        />
         <CardContent>
-          <FormControl>
-            <TextField></TextField>
+          <FormControl fullWidth>
+            <TextField
+              id="outline-email"
+              label="Email"
+              type="email"
+              sx={{ marginBottom: "16px" }}
+            />
           </FormControl>
-          <FormControl>
-              <InputLabel>Password</InputLabel>
-              <OutlinedInput>
-                  <InputAdornment>
-                  <IconButton>
-
-                  </IconButton>
-                  </InputAdornment>
-              </OutlinedInput>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              label="Password"
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    edge="end"
+                  ><Visibility/></IconButton>
+                </InputAdornment>
+              }
+            />
           </FormControl>
         </CardContent>
         <CardActions>
           <Button>Ingresar</Button>
         </CardActions>
       </Card>
-      <Typography>Swap {new Date().getFullYear()} </Typography>
+      <Typography
+        variant="body2"
+        sx={{ color: "#DDDEDF", position: "absolute", bottom: "16px" }}
+      >
+        Swap {new Date().getFullYear()}{" "}
+      </Typography>
     </Container>
   );
 };
