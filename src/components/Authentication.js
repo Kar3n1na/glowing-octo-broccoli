153b65574d366e20286d7a10ce4128c6b5ff0b2d
Karen Tapia <75274720+Kar3n1na@users.noreply.github.com>
2021-10-14 16:29:57 -0500

    Authentication.js authentication component- password visibiliy added and working

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   Button,
@@ -15,69 +15,86 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { Visibility } from "@mui/icons-material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 const Authentication = () => {
-  return (
-    <Container
-      sx={{
-        backgroundColor: "#0061f2",
-        minHeight: "100vh",
-        minWidth: "100%",
-        display: "flex",
-      }}
-    >
-      <Card
-        variant="outlined"
-        sx={{ width: "70%", margin: "auto", borderRadius: "8px" }}
+    const [password, setPassword] = useState({
+      value: "",
+      show: false,
+    });
+  
+    const ShowPasswordToggler = () => {
+      setPassword({ ...password, show: !password.show });
+    };
+  
+    return (
+      <Container
+        sx={{
+          backgroundColor: "#0061f2",
+          minHeight: "100vh",
+          minWidth: "100%",
+          display: "flex",
+        }}
       >
-        <CardHeader
-          sx={{
-            textAlign: "center",
-            backgroundColor: "#F8F9F9",
-            borderBottom: "1px solid #DDDEDF",
-          }}
-          title="Login"
-        />
-        <CardContent>
-          <FormControl fullWidth>
-            <TextField
-              id="outline-email"
-              label="Email"
-              type="email"
-              sx={{ marginBottom: "16px" }}
-            />
-          </FormControl>
-          <FormControl fullWidth>
-            <InputLabel htmlFor="outlined-adornment-password">
-              Password
-            </InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              label="Password"
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    edge="end"
-                  ><Visibility/></IconButton>
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-        </CardContent>
-        <CardActions>
-          <Button>Ingresar</Button>
-        </CardActions>
-      </Card>
-      <Typography
-        variant="body2"
-        sx={{ color: "#DDDEDF", position: "absolute", bottom: "16px" }}
-      >
-        Swap {new Date().getFullYear()}{" "}
-      </Typography>
-    </Container>
-  );
-};
-
-export default Authentication;
+        <Card
+          variant="outlined"
+          sx={{ width: "70%", margin: "auto", borderRadius: "8px" }}
+        >
+          <CardHeader
+            sx={{
+              textAlign: "center",
+              backgroundColor: "#F8F9F9",
+              borderBottom: "1px solid #DDDEDF",
+            }}
+            title="Login"
+          />
+          <CardContent>
+            <FormControl fullWidth>
+              <TextField
+                id="outline-email"
+                label="Email"
+                type="email"
+                sx={{ marginBottom: "16px" }}
+              />
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                value={password.value}
+                label="Password"
+                type={password.show ? "text" : "password"}
+                onChange={(e) =>
+                  setPassword({ ...password, value: e.target.value })
+                }
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={ShowPasswordToggler}
+                      aria-label="toggle password visibility"
+                      edge="end"
+                    >
+                      {password.show ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+          </CardContent>
+          <CardActions>
+            <Button>Ingresar</Button>
+          </CardActions>
+        </Card>
+        <Typography
+          variant="body2"
+          sx={{ color: "#DDDEDF", position: "absolute", bottom: "16px" }}
+        >
+          Swap {new Date().getFullYear()}
+        </Typography>
+      </Container>
+    );
+  };
+  export default Authentication;
+  
